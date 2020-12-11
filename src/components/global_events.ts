f5c824e897b8500b3727f0b844706bea203ba80f
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-11 11:45:25 -0300

    added documentation about local links

--- src/components/global_events.ts
+++ src/components/global_events.ts
@@ -1,5 +1,13 @@
 import GlobalState, { PageType } from "./global_state";
 
+/**
+ * If the link is local, empty the `href` attribute and add a new one called 
+ * `localLink`. For example:
+ * 
+ * ```html
+ * <a href="" localLink="/local/link/path">Local Link</a>
+ * ```
+ */
 export class GlobalEvents {
   private globalState: GlobalState = new GlobalState();
   private ev: Event = new Event("none");
