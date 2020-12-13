474c6ed9f3077b01af4c9c2f7d047b0e71885105
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-13 11:24:10 -0300

    created an article footer

---
+++ src/components/article_footer.ts
@@ -0,0 +1,50 @@
+export default class ArticleFooterElement extends HTMLElement {
+  static readonly tag: string = "article-footer";
+
+  private static readonly template: string = `
+    <style>
+      hr {
+        margin-left: 0px;
+        border: none;
+        max-width: 150px;
+        width: 35vw;
+        height: 0.75px;
+        color: var(--text-color);
+        background-color: var(--text-color);
+      }
+      a {
+        color: #95c379;
+        text-decoration: none;
+      }
+    </style>
+    <br />
+    <br />
+    <br />
+    <hr />
+    <footer></footer>
+  `;
+
+  constructor() {
+    super();
+
+    const template: HTMLTemplateElement = document.createElement("template");
+    template.innerHTML = ArticleFooterElement.template;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot!.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    const footnotes: NodeListOf<HTMLElement> = document.querySelectorAll(
+      "sub"
+    )!;
+    const footer: HTMLElement = this.shadowRoot!.querySelector("footer")!;
+
+    const footnotesLength: number = footnotes.length;
+
+    for (let i = 0; i < footnotesLength; i++) {
+      footer.appendChild(footnotes[i]);
+      footer.append(document.createElement("br"));
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import ArticleFooterElement from "./components/article_footer";
 import FootnoteElement from "./components/footnote";
 import LocalLinkElement from "./components/local_link";
 
 customElements.define(LocalLinkElement.tag, LocalLinkElement);
 customElements.define(FootnoteElement.tag, FootnoteElement);
+customElements.define(ArticleFooterElement.tag, ArticleFooterElement);
