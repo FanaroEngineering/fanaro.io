19cf47a938e94595a6773ef31bdc4d971bd192f6
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-13 14:55:32 -0300

    added an element for the nav bar

---
+++ src/components/fanaro_nav.ts
@@ -0,0 +1,40 @@
+export default class FanaroNavElement extends HTMLElement {
+  static readonly tag: string = "fanaro-nav";
+
+  private static readonly template: string = `
+    <style>
+      a {
+        display: inline-flex;
+        align-items: center;
+        font-size: 1.5rem;
+        color: var(--text-color);
+        text-decoration: none;
+      }
+      svg {
+        padding-right: 10px;
+      }
+    </style>
+    <nav>
+      <a href="/">
+        <svg width="50" height="50">
+          <image width="50" height="50" href="../../assets/favicon.svg" />
+        </svg>
+        <h1>fanaro.io</h1>
+      </a>
+    </nav>
+  `;
+
+  constructor() {
+    super();
+
+    const template: HTMLTemplateElement = document.createElement("template");
+    template.innerHTML = FanaroNavElement.template;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot!.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    document.body.prepend(this);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import ArticleFooterElement from "./components/article_footer";
+import FanaroNavElement from "./components/fanaro_nav";
 import FootnoteElement from "./components/footnote";
 import LocalLinkElement from "./components/local_link";
 
 customElements.define(LocalLinkElement.tag, LocalLinkElement);
 customElements.define(FootnoteElement.tag, FootnoteElement);
 customElements.define(ArticleFooterElement.tag, ArticleFooterElement);
+customElements.define(FanaroNavElement.tag, FanaroNavElement);
