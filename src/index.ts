5d6d2b7cf3e9af284a11ef1cb39653c87b32ae4e
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-13 21:34:54 -0300

    more programmatic nav bar and footer

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,11 @@ customElements.define(LocalLinkElement.tag, LocalLinkElement);
 customElements.define(FootnoteElement.tag, FootnoteElement);
 customElements.define(ArticleFooterElement.tag, ArticleFooterElement);
 customElements.define(FanaroNavElement.tag, FanaroNavElement);
+
+window.addEventListener("DOMContentLoaded", (_: Event) => {
+  document.body.prepend(document.createElement("fanaro-nav"));
+
+  const article: HTMLElement | null = document.querySelector("article");
+  if (document.querySelector("article") != null)
+    article!.append(document.createElement("article-footer"));
+});
