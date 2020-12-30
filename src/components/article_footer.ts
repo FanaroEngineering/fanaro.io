c87aed183efe6c11c26313f4ef25e15d37d9ba4f
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-29 21:47:58 -0300

    no more shadowRoot or template for the ArticleFooter

--- src/components/article_footer.ts
+++ src/components/article_footer.ts
@@ -1,24 +1,7 @@
 export default class ArticleFooter extends HTMLElement {
   static readonly tag: string = "article-footer";
 
-  private static readonly template: string = `
-    <style>
-      hr {
-        margin-left: 0px;
-        border: none;
-        max-width: 150px;
-        width: 35vw;
-        height: 0.75px;
-        color: var(--text-color);
-        background-color: var(--text-color);
-      }
-      
-      a {
-        color: #95c379;
-        text-decoration: none;
-      }
-    </style>
-    
+  private static readonly template: string = `    
     <br />
     <br />
     <br />
@@ -28,19 +11,14 @@ export default class ArticleFooter extends HTMLElement {
 
   constructor() {
     super();
-
-    const template: HTMLTemplateElement = document.createElement("template");
-    template.innerHTML = ArticleFooter.template;
-
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot!.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
+    this.innerHTML = ArticleFooter.template;
     const footnotes: NodeListOf<HTMLElement> = document.querySelectorAll(
       "sub"
     )!;
-    const footer: HTMLElement = this.shadowRoot!.querySelector("footer")!;
+    const footer: HTMLElement = this.querySelector("footer")!;
     const footnotesLength: number = footnotes.length;
     for (let i = 0; i < footnotesLength; i++) {
       footer.appendChild(footnotes[i]);
