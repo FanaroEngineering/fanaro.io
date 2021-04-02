3e4a642523e836f17dc6c1cd693443f23198ce32
Philippe Fanaro <philippefanaro@gmail.com>
2021-04-02 07:58:01 -0300

    small improvements to the script for new article creation | boilerplate for the apocalipse cast article

--- tools/new_article.sh
+++ tools/new_article.sh
@@ -11,12 +11,12 @@ cd articles || exit 1
 
 [ ! -d "$ARTICLE_NAME" ] && mkdir "$ARTICLE_NAME"
 
-cd "$ARTICLE_NAME" || exit 1
+cd "$ARTICLE_NAME" || exit 2
 
 [ ! -f "$ARTICLE_NAME" ] && touch "$ARTICLE_NAME.html"
 
 cd ../..
 
-printf "<url><loc>https://fanaro.io/articles/$ARTICLE_NAME/$ARTICLE_NAME.html</loc></url>" >> sitemap.xml
+printf "\n<url><loc>https://fanaro.io/articles/$ARTICLE_NAME/$ARTICLE_NAME.html</loc></url>" >> sitemap.xml
 
 exit 0
