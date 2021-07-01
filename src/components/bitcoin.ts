1fc49cb576f0a1fa394ada5e853a472bf700aa84
Philippe Fanaro <philippefanaro@gmail.com>
2021-06-30 23:43:41 -0300

    extracted the Bitcoin button into a component

--- src/components/bitcoin.ts
+++ src/components/bitcoin.ts
@@ -1,8 +1,18 @@
 export default class Bitcoin extends HTMLElement {
   static readonly tag: string = "bit-coin";
 
+  private static readonly walletAddr: string =
+    "zpub6o1BausLvdMgM3ebTGvUAcXnb965XnenGw5ij8gVWHvaDyKxBCUHi3yg2ZbTXE5XnZrfQTLHxdQDnp5UfbwSUP92awRXc8nV57JJzXcqq6M";
+
+  private static readonly helperMsg: string =
+    "Copy the following to your clipboard: (Ctrl+C)";
+
   private static readonly template: string = `
-    <p></p>
+    <button>
+      <svg>
+        <image href="../../assets/bitcoin.svg"></image>
+      </svg>
+    </button>
   `;
 
   constructor() {
@@ -11,5 +21,10 @@ export default class Bitcoin extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = Bitcoin.template;
+    this.onclick = this.onClick;
   }
+
+  private onClick = (): void => {
+    window.prompt(Bitcoin.helperMsg, Bitcoin.walletAddr);
+  };
 }
